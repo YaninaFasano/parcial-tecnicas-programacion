a28e12e8e3164ca10598af6de4e94cd4034480d8
YaninaFasano <yani.693@hotmail.com>
2017-11-15 01:50:29 -0300

    se completa ejercicio.

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,12 +1,26 @@
-def rotarPalabra(palabra):
-    rotacion = []
+def condiciones(cadena):
+    verificacion = []
     lista = "no_espacio"
-    for letra in palabra:
-        if palabra == "":
-            rotacion.append(letra)
+    for letra in cadena:
+        if letra == " ":
+            verificacion.append(letra)
         else:
-            rotacion.append(lista)
+            verificacion.append(lista)
+    if lista not in verificacion or len(cadena) < 1:
+        return False
 
+def rotarPalabra(palabra):
+    if condiciones(palabra) == False:
+        return []
+    lista = []
+    rotacion = len(palabra)
+    ultimaLetra = -1
+    for vueltas in range(rotacion):
+        rotar = palabra[ultimaLetra:] + palabra[:ultimaLetra]
+        lista.append(rotar)
+        ultimaLetra = ultimaLetra - 1
+    lista = lista[::-1]
+    return lista
 
 
 
